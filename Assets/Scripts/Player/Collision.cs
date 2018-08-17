55a811589256d27d90bb6388fc671a1dbcd5eb5b
MONKEYDALYN\a <gogoasealin@gmail.com>
2018-08-17 18:31:29 +0300

    Movement

---
+++ Assets/Scripts/Player/Collision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+
+    private void OnCollisionEnter(UnityEngine.Collision other)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    [SerializeField]private Rigidbody rb;
+    public float speed = 10f;
+
+
+
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * speed;
+    }
+
+
+
+
+}
